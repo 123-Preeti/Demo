75e45e17473063d0a7eed5443088d01af0a34da3
Preeti <jhapreeti872@gmail.com>
2025-09-12 10:14:00 +0530

    add

--- java basics/java/op3.java
+++
@@ -1,7 +0,0 @@
-public class op3 {
-    public static void main(String[] args) {
-        int a =10, b = 1, c = 2;
-        int d = a > b & a > c ? a : b > c ? b: c;
-        System.out.println(d);
-    }}
-
